1b0e87213cfdc7352d0b6e5703a53bff98b9711f
djamey <djamey87@gmail.com>
2019-08-23 22:46:05 +0100

    initial boiler plate with redux

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className="ui container grid">
+      <div className="ui row">
+        <div className="column eight wide">
+          <p>test</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
