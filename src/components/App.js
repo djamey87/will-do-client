966dfa9eb0d78e0b0aac721ca9865a658bb01aeb
djamey <djamey87@gmail.com>
2019-08-23 23:02:20 +0100

    porting thunk blog structure from udemy course, as an improved boiler plate for testing

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,10 @@
 import React from 'react';
+import PostList from './PostList';
 
 const App = () => {
   return (
-    <div className="ui container grid">
-      <div className="ui row">
-        <div className="column eight wide">
-          <p>test</p>
-        </div>
-      </div>
+    <div className="ui container">
+      <PostList />
     </div>
   );
 };
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,23 +1,8 @@
 import { combineReducers } from 'redux';
-
-const songsReducer = () => {
-  return [
-    { title: 'No Scrubs', duration: '4:05' },
-    { title: 'Macarena', duration: '2:30' },
-    { title: 'All Star', duration: '3:15' },
-    { title: 'I Want it That Way', duration: '1:45' }
-  ];
-};
-
-const selectedSongReducer = (selectedSong = null, action) => {
-  if (action.type === 'SONG_SELECTED') {
-    return action.payload;
-  }
-
-  return selectedSong;
-};
+import postsReducer from './postsReducer';
+import usersReducer from './usersReducer';
 
 export default combineReducers({
-  songs: songsReducer,
-  selectedSong: selectedSongReducer
+  posts: postsReducer,
+  users: usersReducer
 });
