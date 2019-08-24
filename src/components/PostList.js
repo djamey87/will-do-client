5cfec38049b817d8aebef30cc970db87868fcfe1
djamey <djamey87@gmail.com>
2019-08-25 00:10:10 +0100

    fetchUsers action added for todo api, and implemented in post component

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,38 +1,39 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { fetchPostsAndUsers } from '../actions';
+import { fetchPostsAndUsers, fetchUsers } from '../actions';
 import UserHeader from './UserHeader';
 
 class PostList extends React.Component {
-  componentDidMount() {
-    this.props.fetchPostsAndUsers();
-  }
+	componentDidMount() {
+		// this.props.fetchPostsAndUsers();
+		this.props.fetchUsers();
+	}
 
-  renderList() {
-    return this.props.posts.map(post => {
-      return (
-        <div className="item" key={post.id}>
-          <div className="content">
-            <div className="description">
-              <h2>{post.title}</h2>
-            </div>
-            <UserHeader userId={post.userId} />
-          </div>
-        </div>
-      );
-    });
-  }
+	renderList() {
+		return this.props.posts.map(post => {
+			return (
+				<div className="item" key={post.id}>
+					<div className="content">
+						<div className="description">
+							<h2>{post.title}</h2>
+						</div>
+						<UserHeader userId={post.userId} />
+					</div>
+				</div>
+			);
+		});
+	}
 
-  render() {
-    return <div className="ui relaxed divided list">{this.renderList()}</div>;
-  }
+	render() {
+		return <div className="ui relaxed divided list">{this.renderList()}</div>;
+	}
 }
 
 const mapStateToProps = state => {
-  return { posts: state.posts };
+	return { posts: state.posts };
 };
 
 export default connect(
-  mapStateToProps,
-  { fetchPostsAndUsers }
+	mapStateToProps,
+	{ fetchPostsAndUsers, fetchUsers }
 )(PostList);
--- src/reducers/usersReducer.js
+++ src/reducers/usersReducer.js
@@ -1,8 +1,10 @@
 export default (state = [], action) => {
-  switch (action.type) {
-    case 'FETCH_USER':
-      return [...state, action.payload];
-    default:
-      return state;
-  }
+  	switch (action.type) {
+    	case 'FETCH_USER':
+      		return [...state, action.payload];
+	  	case 'FETCH_USERS':
+        	return [...state, action.payload];
+    	default:
+      		return state;
+  	}
 };
