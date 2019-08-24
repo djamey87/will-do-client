a803d7a7144573779b7443b24259fd328afc2c23
djamey <djamey87@gmail.com>
2019-08-25 00:09:40 +0100

    amending url for local server (todo json api) including todo api in actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,26 +1,35 @@
 import _ from 'lodash';
 import jsonPlaceholder from '../apis/jsonPlaceholder';
+import todo from '../apis/todo';
 
 export const fetchPostsAndUsers = () => async (dispatch, getState) => {
-  await dispatch(fetchPosts());
+	await dispatch(fetchPosts());
 
-  _.chain(getState().posts)
-    .map('userId')
-    .uniq()
-    .forEach(id => dispatch(fetchUser(id)))
-    .value();
+	_.chain(getState().posts)
+		.map('userId')
+		.uniq()
+		.forEach(id => dispatch(fetchUser(id)))
+		.value();
 };
 
 export const fetchPosts = () => async dispatch => {
-  const response = await jsonPlaceholder.get('/posts');
+	const response = await jsonPlaceholder.get('/posts');
 
-  dispatch({ type: 'FETCH_POSTS', payload: response.data });
+	dispatch({ type: 'FETCH_POSTS', payload: response.data });
 };
 
 export const fetchUser = id => async dispatch => {
-  const response = await jsonPlaceholder.get(`/users/${id}`);
+	const response = await jsonPlaceholder.get(`/users/${id}`);
 
-  dispatch({ type: 'FETCH_USER', payload: response.data });
+	dispatch({ type: 'FETCH_USER', payload: response.data });
+};
+
+export const fetchUsers = () => async dispatch => {
+	const response = await todo.get(`/users`);
+
+	console.log(`users ${JSON.stringify(response.data)}`);
+
+	dispatch({ type: 'FETCH_USERS', payload: response.data });
 };
 
 // export const fetchUser = id => dispatch => _fetchUser(id, dispatch);
--- src/apis/todo.js
+++ src/apis/todo.js
@@ -3,8 +3,8 @@ import axios from 'axios';
 // TODO: create a basic server, to store the following
 // 1. tasks
 // 2. intervals
-// 3. users 
+// 3. users
 
 export default axios.create({
-  baseURL: 'https://jsonplaceholder.typicode.com'
+  	baseURL: 'http://10.0.33.34:3000'
 });
