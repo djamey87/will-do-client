5a3680ea621aa3be929633b22f97987e57321ffb
djamey <djamey87@gmail.com>
2019-08-29 23:26:54 +0100

    removing redundancies

--- src/actions/user.js
+++ src/actions/user.js
@@ -1,4 +1,4 @@
-import _ from 'lodash';
+// import _ from 'lodash';
 import todo from '../apis/todo';
 
 // example of action chaining
--- src/reducers/usersReducer.js
+++ src/reducers/usersReducer.js
@@ -1,7 +1,5 @@
 export default (state = [], action) => {
 	switch (action.type) {
-		case 'FETCH_USER':
-			return [...state, action.payload];
 		case 'FETCH_USER_SESSION':
 			return [...state, action.payload];
 		case 'FETCH_USERS':
