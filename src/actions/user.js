9cbcc76c104ac8212d2575d5db94cc921d30d4b5
djamey <djamey87@gmail.com>
2019-09-06 17:02:35 +0100

    moved auth related actions to separate file

--- src/actions/user.js
+++ src/actions/user.js
@@ -1,6 +1,8 @@
 // import _ from 'lodash';
 import todo from '../apis/todo';
 
+import { FETCH_USERS } from '../constants/user';
+
 // example of action chaining
 // export const fetchPostsAndUsers = () => async (dispatch, getState) => {
 // 	await dispatch(fetchPosts());
@@ -12,45 +14,12 @@ import todo from '../apis/todo';
 // 		.value();
 // };
 
-export const login = ({ email, password }) => async dispatch => {
-	console.log(`users login ${email} ${password}`);
-
-	const response = await todo.post(`/users/login`, { user: { email, password } });
-
-	console.log(`users login ${JSON.stringify(response.data)}`);
-
-	dispatch({ type: 'LOGIN', payload: response.data });
-};
-
-export const logout = () => async dispatch => {
-	const response = await todo.get(`/users/logout`);
-
-	console.log(`users logout ${JSON.stringify(response.data)}`);
-
-	dispatch({ type: 'LOGOUT', payload: response.data });
-};
-
-// get the locally signed in user
-export const fetchUserSession = () => async dispatch => {
-	let response;
-
-	try {
-		response = await todo.get(`/users/currentSession`);
-	} catch (error) {
-		response = { data: { user: null } };
-	}
-
-	console.log(`user session ${JSON.stringify(response.data)}`);
-
-	dispatch({ type: 'FETCH_USER_SESSION', payload: response.data });
-};
-
 export const fetchUsers = () => async dispatch => {
 	const response = await todo.get(`/users`);
 
 	console.log(`users ${JSON.stringify(response.data)}`);
 
-	dispatch({ type: 'FETCH_USERS', payload: response.data });
+	dispatch({ type: FETCH_USERS, payload: response.data });
 };
 
 // export const fetchUser = id => dispatch => _fetchUser(id, dispatch);
