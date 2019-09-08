b2190b13de02008a340c0fb0cb2351eb8641cd1e
djamey <djamey87@gmail.com>
2019-09-08 17:05:40 +0300

    adding a plugin for the new task form reducer to ensure the form is cleared on submit

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,11 +2,25 @@ import { combineReducers } from 'redux';
 
 import { reducer as formReducer } from 'redux-form';
 
+import { CREATE_TASK } from '../constants/task';
+
 import tasks from './tasks';
 import users from './users';
 
 export default combineReducers({
 	tasks: tasks,
 	users: users,
-	form: formReducer,
+
+	// handle form submissions
+	form: formReducer.plugin({
+		createTask: (state, action) => {
+			// <------ 'account' is name of form given to reduxForm()
+			switch (action.type) {
+				case CREATE_TASK:
+					return undefined; // <--- blow away form data
+				default:
+					return state;
+			}
+		},
+	}),
 });
