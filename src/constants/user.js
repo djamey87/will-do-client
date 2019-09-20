15fea15e2cd219db0aa3c44d6c026a82b9454c12
djamey <djamey87@gmail.com>
2019-09-20 21:11:23 +0100

    utilising constants in reducer

--- src/constants/user.js
+++ src/constants/user.js
@@ -1,1 +1,4 @@
+export const LOGIN = 'LOGIN';
+export const LOGOUT = 'LOGOUT';
+export const FETCH_USER_SESSION = 'FETCH_USER_SESSION';
 export const FETCH_USERS = 'FETCH_USERS';
--- src/reducers/users.js
+++ src/reducers/users.js
@@ -1,24 +1,30 @@
-export default (state = {}, action) => {
+import { LOGIN, LOGOUT, FETCH_USER_SESSION, FETCH_USERS } from '../constants/user';
+
+const defaultState = {
+	localUser: false,
+};
+
+export default (state = defaultState, action) => {
 	switch (action.type) {
-		case 'LOGIN':
-			console.log('LOGIN', action);
+		case LOGIN:
+			// console.log(`${action.type}`, action);
 			return {
 				...state,
 				localUser: action.payload.user,
 			};
-		case 'LOGOUT':
-			console.log('LOGOUT', action);
+		case LOGOUT:
+			// console.log(`${action.type}`, action);
 			return {
 				...state,
 				localUser: null,
 			};
-		case 'FETCH_USER_SESSION':
-			console.log('FETCH_USER_SESSION', action);
+		case FETCH_USER_SESSION:
+			console.log(`${action.type}`, action);
 			return {
 				...state,
 				localUser: action.payload.user,
 			};
-		case 'FETCH_USERS':
+		case FETCH_USERS:
 			return state;
 		default:
 			return state;
